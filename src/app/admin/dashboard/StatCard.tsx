2decc39326c029c0a3a317d0f382e815d0e4db67
Chuacx13 <chuachongxuan@gmail.com>
2025-01-12 18:28:09 +0800

    Debug Type error: Cannot find namespace 'JSX' in StatCard

--- src/app/admin/dashboard/StatCard.tsx
+++ src/app/admin/dashboard/StatCard.tsx
@@ -1,5 +1,5 @@
 import { LucideIcon } from "lucide-react";
-import React from "react";
+import React, { JSX } from "react";
 import { Card } from "antd";
 import { Text } from "@/app/(components)/Text";
 // import { LineChartOutlined } from "@ant-design/icons";
