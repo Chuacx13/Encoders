cc63bc92e1e32144954ae4ed1a4cb80a920c7556
Chuacx13 <chuachongxuan@gmail.com>
2025-01-14 22:27:57 +0800

    Added create account api

---
+++ src/app/api/createAccount/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { initAdmin } from "@/firebase/firebaseAdmin";
+import admin from 'firebase-admin';
+
+export async function POST(req: Request) {
+    await initAdmin();
+
+    try {
+        const body = await req.json();
+        const { email, phoneNumber, password, role } = body
+
+        const isAdmin = role !== 'resident';
+        console.log(role);
+        const user = await admin.auth().createUser({
+            email,
+            password, 
+            phoneNumber
+        })
+
+        await admin.auth().setCustomUserClaims(user.uid, {
+            admin: isAdmin, 
+            firstTimeLogin: true
+        });
+
+        return NextResponse.json({
+            message: 'First Time Login Claim set successfully',
+        });
+    } catch (error) {
+        console.error('Error creating account.', error);
+        return NextResponse.json(
+            { message: 'Failed to Create Account' }, 
+            { status: 500 }
+        )
+    }
+} 
