d0dcab09afcb9b01d9ba167586bbae5bb6aff45a
Chuacx13 <chuachongxuan@gmail.com>
2025-01-15 00:41:11 +0800

    Added users and residents interface

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,17 +1,29 @@
 export interface Voucher {
-    id: number;
-    name: string;
-    value: string;
-    validUntil: string;
-    status: string;
-    redeemedOn?: string | null;
-  }
-  
-  export interface Item {
-    id: number;
-    name: string;
-    price: string | number;
-    datePurchased: string;
-    category: string;
-  }
-  
+  id: number;
+  name: string;
+  value: string;
+  validUntil: string;
+  status: string;
+  redeemedOn?: string | null;
+}
+
+export interface Item {
+  id: number;
+  name: string;
+  price: string | number;
+  datePurchased: string;
+  category: string;
+}
+
+export interface User { 
+  id: string;
+  email: string;
+  name: string;
+  phoneNumber: string;
+  role: string;
+}
+
+export interface Resident extends User {
+  purchasedItems: Array<number>;
+  voucher: Array<number>;
+}
