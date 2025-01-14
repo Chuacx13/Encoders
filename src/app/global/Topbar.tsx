cf7eae605daee83d1ec00fdad1b37d9c9cb3a9a6
Chuacx13 <chuachongxuan@gmail.com>
2025-01-14 23:19:33 +0800

    Added logout functionality to Topbar

--- src/app/global/Topbar.tsx
+++ src/app/global/Topbar.tsx
@@ -1,11 +1,30 @@
+"use client"
+
 import Image from "next/image";
 import avatar from "../../../public/avatar.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import { useRouter } from "next/navigation";
 
 interface TopbarProps {
   userType: "admin" | "resident";
 }
 
+
 export default function Topbar({ userType }: TopbarProps) {
+
+  const router = useRouter();
+
+  const handleLogout = async() => {
+    
+    try {
+      await signOut(auth);
+      router.push('/');
+
+    } catch(error) {
+      console.error(error);
+    }
+  }
   return (
     <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-8 shadow-lg">
       <div className="flex items-center gap-4">
@@ -14,7 +33,7 @@ export default function Topbar({ userType }: TopbarProps) {
       </div>
       <div className="flex items-center gap-4">
         <span>{`Welcome, ${userType === "admin" ? "Admin" : "Resident"}!`}</span>
-        <button className="bg-white text-blue-600 px-4 py-2 rounded-md hover:bg-gray-200">
+        <button onClick={handleLogout} className="bg-white text-blue-600 px-4 py-2 rounded-md hover:bg-gray-200">
           Logout
         </button>
       </div>
