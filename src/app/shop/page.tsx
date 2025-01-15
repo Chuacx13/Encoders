1dbf8228e439d3ec810d71e1eb1b1f5d4b1777bb
Respirayson <raysonyeap4c@gmail.com>
2025-01-16 03:14:29 +0800

    Add carousel functionality to shop

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay"
 import Billboard from "../(components)/Billboard";
 import ProductList from "../(components)/ProductList";
 import Container from "../(components)/ui/Container";
@@ -11,7 +13,19 @@ export default function ShopPage() {
 
   return (
         <>
-        <Billboard data={mockBillboard} />
+        <Carousel plugins={[Autoplay({
+          delay: 5000,
+        })]} opts={{ loop: true, watchDrag: false }}>
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <Billboard data={mockBillboard} />
+                </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="left-12" />
+          <CarouselNext className="right-12" />
+        </Carousel>
         <Container>
       <div className="pb-10 space-y-10">
         <Categories />
